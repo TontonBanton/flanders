def0dfd5b208b41b6d258532578cdc5c7fac0452
TontonBanton <pancla13ulas@gmail.com>
2024-05-16 10:41:06 +0800

    Navigation Bar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <div class="border border-white justify-between hidden w-10/12 h-16 mx-auto my-10 lg:flex">
+    <div class="justify-between hidden w-10/12 h-16 mx-auto my-10 lg:flex">
+
       <nav class="self-center w-5/12">
         <ul class="flex">
           <li class="mr-8 text-lg text-white cursor-pointer"><router-link to="/">Portfolio</router-link></li>
@@ -8,6 +9,27 @@
           <li class="mr-8 text-lg text-white cursor-pointer"><router-link to="/contact">Contact Me</router-link></li>
         </ul>
       </nav>
+
+      <router-link to="/">
+        <img src="@/assets/zen.png" class="self-center w-60 h-60">
+      </router-link>
+
+
+      <div class="flex justify-end w-5/12 h-16">
+        <a href="https://www.instagram.com/the_shred_flanders/" target="_blank" class="my-auto cursor-pointer ml-7">
+          <i class="text-2xl text-white fa-brands fa-instagram"></i>
+        </a>
+        <a href="https://www.facebook.com/profile.php?id=100083959629324" target="_blank" class="my-auto cursor-pointer ml-7">
+          <i class="text-2xl text-white fa-brands fa-facebook"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/shred-flanders-356896246/" target="_blank" class="my-auto cursor-pointer ml-7">
+          <i class="text-2xl text-white fa-brands fa-linkedin"></i>
+        </a>
+        <a href="https://twitter.com/ShredFlanders4" target="_blank" class="my-auto cursor-pointer ml-7">
+          <i class="text-2xl text-white fa-brands fa-twitter"></i>
+        </a>
+
+      </div>
     </div>
     <router-view/>
   </div>
